e82909687d67fcfd2ed5e52fdd97ea34d64cf074
naaih <a.naaih@outlook.com>
2023-05-02 13:53:54 +0500

    OS - Project One

---
+++ Pintos/tests/threads/print-name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/init.h"
+
+void test_uwe_print_name(void)
+{
+
+	printf("UWE you ID: S1800001\n");
+	printf("UWE Department: VillaCollege SCS!\n");
+	printf("UWE City: Male'\n");
+	printf("UWE Maldives\n");
+
+
+}
+
+void test_print_name(void)
+{
+
+	printf("UWE Print Name!\n");
+}
+
